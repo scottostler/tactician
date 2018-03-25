09060e192301730cda22283e1bbb4f2efee9c6be
Scott Ostler <scott.ostler@gmail.com>
2018-03-25 10:30:59 -0700

    Include non-province pile count in turn summary

--- src/game_logging.rs
+++ src/game_logging.rs
@@ -3,10 +3,9 @@ use cards;
 use game::{EvalContext, Game, EMPTY_PILES_FOR_GAME_END};
 
 impl Game {
-
     pub fn print_turn_start_summary(&self, ctx: &mut EvalContext) {
         if !ctx.debug {
-            return
+            return;
         }
 
         let ref player = self.players[self.active_player.0 as usize];
@@ -14,7 +13,7 @@ impl Game {
 
         let vp_and_turns = self.player_vp_and_turns();
         let player_vp_pairs = self.players.iter().zip(vp_and_turns);
-        
+
         for (player, (vp, _)) in player_vp_pairs {
             println!("- {}: {} VP", player.name, vp)
         }
@@ -26,14 +25,32 @@ impl Game {
             println!("- {} Provinces left", provinces_left);
         }
 
-        // TODO: Needs sorted_by_key from itertools update
-        // let piles_to_empty = self.piles
-        //     .iter()
-        //     .filter(|&(card, count)| *card != cards::PROVINCE.identifier)
-        //     .sorted_by_key(|(card, count)| -count)  
-        //     .take(EMPTY_PILES_FOR_GAME_END);
+        let non_province_pile_counts = self.piles
+            .iter()
+            .filter(|&(card, _)| *card != cards::PROVINCE.identifier)
+            .sorted_by_key(|&(_, count)| count);
 
-        // println!()
+        let cards_to_empty_string = non_province_pile_counts
+            .iter()
+            .take(EMPTY_PILES_FOR_GAME_END as usize)
+            .map(|&(card, count)| {
+                let card = cards::lookup_card(card);
+                if *count == 0 {
+                    format!("**{}**", card.name)
+                } else {
+                    format!("{} ({})", card.name, count)
+                }
+            })
+            .join(", ");
+
+        let count_to_empty: i32 = non_province_pile_counts
+            .iter()
+            .take(EMPTY_PILES_FOR_GAME_END as usize)
+            .map(|&(_, count)| count)
+            .sum();
+
+        println!("- {} other cards to empty piles", count_to_empty);
+        println!("  {}", cards_to_empty_string);
+        println!();
     }
-
 }
